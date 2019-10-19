ff5f6eee7dde07b7c70fd2fdfe25b3b01be134ad
John Gomez <silva.johngomes@gmail.com>
2019-10-19 15:38:00 -0300

    primeiro commit

---
+++ js/download.js
@@ -0,0 +1,30 @@
+var regex_url= /("http[s]?:\/\/)?instagram.fcgh7-1(.*")/g;
+ 
+$('button').click(function(){
+    var url = $('input').val(); 
+
+    $.get(url, function(data, status){
+        var html = data.toString();
+        var tag = html.match(regex_url);
+        var url_video= tag[1].replace('"', '').replace('"', '');    
+
+        fetch(url_video)
+            .then(resp => resp.blob())
+            .then(blob => {
+                const url = window.URL.createObjectURL(blob);
+                const a = document.createElement('a');
+                a.style.display = 'none';
+                a.href = url;
+                
+                // the filename you want
+                a.download = 'video.mp4';
+                document.body.appendChild(a);
+                a.click();
+                window.URL.revokeObjectURL(url);
+                alert('Download Realizado com Sucesso!'); // or you know, something with better UX...
+            })
+        .catch(() => alert('Poxa vida, Ocorreu um erro :('));
+    });
+}
+);
+
